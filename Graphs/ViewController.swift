ac4cfcafa8297e96cb13160c101ad3f439c6f3e4
Taylor H. Gilbert <taylorg@192.168.1.2>
2014-11-26 21:39:04 -0800

    Initial Commit

---
+++ Graphs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Graphs
+//
+//  Created by Taylor H. Gilbert on 11/26/14.
+//  Copyright (c) 2014 Taylor H. Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
